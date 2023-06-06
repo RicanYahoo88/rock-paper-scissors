c9d13c30e97ee671e3c053c51ecf4850c2cbd1fa
RicanYahoo88 <132402469+RicanYahoo88@users.noreply.github.com>
2023-06-06 15:30:24 +0000

    changed the script
    simplified script

---
+++ script.js
@@ -0,0 +1,13 @@
+function playRound() {
+  //get the players choice
+  var playerSelection = prompt("Rock? Paper? Scissors? Make Your Choice", "Make Your Choice: ");
+
+  var computerSelection = Math.floor(Math.random() * 3);
+  
+  if (playerSelection != null) {
+        return playerSelection;
+}
+
+console.log(playRound("You chose " + playerSelection + " and the computer chose " + computerSelection));
+  
+}

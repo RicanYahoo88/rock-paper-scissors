32cdf782a1a6f4484f845f52a0566808ab8f4eef
RicanYahoo88 <132402469+RicanYahoo88@users.noreply.github.com>
2023-06-06 13:13:49 -0400

    tried a different script to check errors

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ function playRound() {
   //get the players choice
   var playerSelection = prompt("Rock? Paper? Scissors? Make Your Choice");
 
-  if (playerSelection != null) {
-    return playerSelection;
-  }
 
+  //make computers choice random
   var computerSelection = Math.floor(Math.random() * 3);
+  /* if (playerSelection != null) {
+    return playerSelection;
+  } */
 
-  console.log("You chose " + playerSelection + " and the computer chose " + computerSelection);
+  // display the results
+  console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}`);
+
+
+  
 
 }
 
